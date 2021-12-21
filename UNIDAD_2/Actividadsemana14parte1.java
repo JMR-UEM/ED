821cbbb55fc5a14535aa15f52b6ea133e4a52d26
JMR-UEM <jmr_uem@DESKTOP-CQH6OUN>
2021-12-21 20:03:03 +0100

    Subir UNIDAD_2

---
+++ UNIDAD_2/Actividadsemana14parte1.java
@@ -0,0 +1,9 @@
+public class Actividadsemana14parte1 {
+    public static void main(String[] args) {
+        double temp = 0.2d;
+        temp += 0.1f;
+        temp += 0.1f;
+        temp += 0.1f;
+        System.out.println("Dato: " + temp);
+    }
+}
